f964e6d6a2415661e1abd9ea89eabaa31b96ab84
SalahNappi <113444670+SalahNappi@users.noreply.github.com>
2024-04-05 10:40:55 -0300

    first commit

---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Teste de temperatura
+Console.WriteLine("Qual é a temperatura lá fora?");
+double temp = Convert.ToDouble(Console.ReadLine());
+
+if (temp >= 10 && temp <= 25)
+{
+    Console.WriteLine("está é um tempo agradavel lá fora");
+}
+
+else if (temp <= -50 || temp >= 50)
+{
+    Console.WriteLine("NÃO VAI LÁ FORA");
+}
+
+
+Console.ReadKey();
